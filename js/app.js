0482ebeb6771b81f8529d3c67d441d53d85acdf4
John <ashton.basile@gmail.com>
2023-08-11 16:23:45 +0100

    The form submission is now handled

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript loaded');
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleFormSubmit);
 })
+
+const handleFormSubmit = function (event) {
+  event.preventDefault();
+  const newListItem = document.createElement('li');
+  newListItem.textContent = `
+    ${this.title.value},
+    ${this.author.value},
+    ${this.category.value}
+  `
+  const readingList = document.querySelector('#reading-list');
+  readingList.appendChild(newListItem);
+  document.getElementById('new-item-form').reset();
+}
